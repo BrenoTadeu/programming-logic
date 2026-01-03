384ee0e91891802c527d66a004757c76a2e8dd5b
BrenoTadeu <btgs1233@gmail.com>
2026-01-03 01:26:26 -0300

    First commit on the main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
+        // The scanner is the port of the input
         Scanner input = new Scanner(System.in);
 
         System.out.println("Type your age:");
+        //Then the variable stores the Scanner data
         String numDigitado = input.nextLine();
 
+        //The Variable is converting to Integer
         int numConvert = Integer.parseInt(numDigitado);
+        //Condition to know, if age to voting or not
         if(numConvert == 16){
             System.out.println("Okay! Than you can voting!");
         }else{
